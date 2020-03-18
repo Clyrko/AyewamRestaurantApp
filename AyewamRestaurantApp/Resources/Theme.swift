149a1cfb54dd4db2adf1b50b91d56f09aba06549
Jay A <justynadusei@gmail.com>
2020-03-18 16:40:39 +0000

    Theme Set

---
+++ AyewamRestaurantApp/Resources/Theme.swift
@@ -0,0 +1,20 @@
+//
+//  Theme.swift
+//  AyewamRestaurantApp
+//
+//  Created by Jay A. on 3/18/20.
+//  Copyright © 2020 Jay A. All rights reserved.
+//
+
+import UIKit
+
+class Theme {
+    
+    static let mainFontName = "SFProText-Light"
+    
+    static let tint = UIColor(named: "Tint")
+    static let lightAccent = UIColor(named: "LightAccent")
+    static let darkAccent = UIColor(named: "DarkAccent")
+    static let lightBackground = UIColor(named: "LightBackground")
+    static let darkBackground = UIColor(named: "DarkBackground")
+}
