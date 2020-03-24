ea8fffaea072bd1c604eef65c4e5c8b3c0b54f6d
Jay A <justynadusei@gmail.com>
2020-03-24 19:16:44 +0000

    Firestore CRUD

---
+++ AyewamRestaurantApp/FirestoreService.swift
@@ -0,0 +1,19 @@
+//
+//  FirestoreService.swift
+//  AyewamRestaurantApp
+//
+//  Created by Jay A. on 3/24/20.
+//  Copyright © 2020 Jay A. All rights reserved.
+//
+
+import Foundation
+
+class FirestoreService {
+    
+    private init() {}
+    static let shared = FirestoreService()
+    
+    
+    
+    
+}
---
+++ AyewamRestaurantApp/StorageService.swift
@@ -0,0 +1,19 @@
+//
+//  StorageService.swift
+//  AyewamRestaurantApp
+//
+//  Created by Jay A. on 3/24/20.
+//  Copyright © 2020 Jay A. All rights reserved.
+//
+
+import Foundation
+
+class StorageService {
+    
+    private init() {}
+    static let shared = StorageService()
+    
+    
+    
+    
+}
