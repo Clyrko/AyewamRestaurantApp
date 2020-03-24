43c11dbab17e373414acb35375fef3ea49a8caf4
Jay A <justynadusei@gmail.com>
2020-03-24 14:32:38 +0000

    Set controls for image picking

---
+++ AyewamRestaurantApp/View Controllers/NewMenuItemViewController.swift
@@ -0,0 +1,66 @@
+//
+//  NewMenuItemViewController.swift
+//  AyewamRestaurantApp
+//
+//  Created by Jay A. on 3/24/20.
+//  Copyright © 2020 Jay A. All rights reserved.
+//
+
+import UIKit
+
+class NewMenuItemViewController: UIViewController {
+
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var mainImageButton: UIButton!
+    @IBOutlet weak var firstImageButton: UIButton!
+    @IBOutlet weak var secondImageButton: UIButton!
+    @IBOutlet weak var saveButton: UIButton!
+    
+    private var currentButton: UIButton?
+    
+    private lazy var pickerController: UIImagePickerController = {
+        
+        let picker = UIImagePickerController()
+        picker.sourceType = .photoLibrary
+        
+        return picker
+    }()
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func dismissKeyboard() {
+        
+        titleTextField.endEditing(true)
+    }
+    
+    @IBAction func setPhoto(_ sender: UIButton) {
+        
+        currentButton = sender
+        present(pickerController, animated: true)
+    }
+    
+    @IBAction func saveMenuItem() {
+        
+        
+    }
+    
+}
+
+extension NewMenuItemViewController: UIImagePickerControllerDelegate {
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true)
+        guard let image = info[.originalImage] as? UIImage else { return }
+        currentButton?.setImage(image, for: .normal)
+    }
+}
